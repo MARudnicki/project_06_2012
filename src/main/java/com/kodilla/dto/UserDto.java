54342646810f4f4aa64c64273ca872afdd0bec95
Robert Gajewski <robert.gajewski@nask.pl>
2021-06-25 22:49:26 +0200

    OrderController zwracanie danych testowych

--- src/main/java/com/kodilla/dto/UserDto.java
+++ src/main/java/com/kodilla/dto/UserDto.java
@@ -5,11 +5,17 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 public class UserDto {
     private final Long id;
     private final String username;
     private final String status;
-    private final Long userKey;
+    private final BigDecimal userKey;
+
+    public List<OrderDto> getUser() { return new ArrayList<>(); }
 }
--- src/main/java/com/kodilla/ecommercee/OrderController.java
+++ src/main/java/com/kodilla/ecommercee/OrderController.java
@@ -22,7 +22,7 @@ public class OrderController {
     public OrderDto getOrder(@PathVariable Long orderId) {
         return new OrderDto(
                 1L,
-                new UserDto(10L,"user","o",31251L),
+                new UserDto(10L,"user","o",new BigDecimal(31251)),
                 new ProductDto(20L,"kurtka zimowa", "Pellentesque tempus interdum quam ut rhoncus. Donec ullamcorper turpis dolor. Donec euismod pretium eros et eleifend. Aliquam vulputate faucibus lorem non auctor. Vivamus erat turpis, molestie a nisl non, scelerisque luctus enim. Nunc mi mi, laoreet ac mollis nec, pharetra sit amet tortor. Vivamus a bibendum purus.", new BigDecimal(100)),
                 true
         );
@@ -37,12 +37,12 @@ public class OrderController {
     public OrderDto updateOrder(@RequestBody OrderDto orderDto) {
         return new OrderDto(
                 1L,
-                new UserDto(10L,"user","o",31251L),
+                new UserDto(10L,"user","o",new BigDecimal(31251)),
                 new ProductDto(20L,"kurtka zimowa", "Pellentesque tempus interdum quam ut rhoncus. Donec ullamcorper turpis dolor. Donec euismod pretium eros et eleifend. Aliquam vulputate faucibus lorem non auctor. Vivamus erat turpis, molestie a nisl non, scelerisque luctus enim. Nunc mi mi, laoreet ac mollis nec, pharetra sit amet tortor. Vivamus a bibendum purus.", new BigDecimal(100)),
                 true
         );
     }
 
-    @DeleteMapping(value = "deleteOrder")
-    public void deleteOrder(Long OrderId) {}
+    @DeleteMapping(value = "deleteOrder/{orderId}")
+    public void deleteOrder(@PathVariable Long orderId) {}
 }
