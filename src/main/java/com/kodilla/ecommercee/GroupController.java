5dcec1b1cdacb681bf18635081a716606e0eac90
andrzej-t <andrzej.tyrpa@gmail.com>
2021-06-26 09:15:18 +0200

    First changes in JDP210602-2 GroupCotroller and making GroupDto

--- src/main/java/com/kodilla/ecommercee/GroupController.java
+++ src/main/java/com/kodilla/ecommercee/GroupController.java
@@ -1,4 +1,31 @@
 package com.kodilla.ecommercee;
 
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 public class GroupController {
+
+    @GetMapping(value = "getGroups")
+    public List<GroupDto> getGroups() {
+        return new ArrayList<>();
+    }
+
+    @PostMapping(value = "createGroup")
+    public void createGroup(@RequestBody GroupDto groupDto) {
+
+    }
+
+    @GetMapping(value = "getGroup/{groupId}")
+    public GroupDto getGroup(@RequestParam Long groupId) {
+        return new GroupDto(1,"Ubrani a");
+    }
+
+    @PutMapping(value = "updateGroup")
+    public GroupDto updateGroup(@RequestBody GroupDto groupDto) {
+        return new GroupDto(1,"Ubrania");
+    }
+
 }
---
+++ src/main/java/com/kodilla/ecommercee/GroupDto.java
@@ -0,0 +1,13 @@
+package com.kodilla.ecommercee;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GroupDto {
+    private long id;
+    private String name;
+}
