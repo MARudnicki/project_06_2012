2d358e06e26eca2abc531dac0258f43b3a0a5cd0
Jakub <jurak2012@gmail.com>
2021-06-26 18:34:46 +0200

    Updated ProductDto

--- src/main/java/com/kodilla/ecommercee/ProductController.java
+++ src/main/java/com/kodilla/ecommercee/ProductController.java
@@ -17,7 +17,7 @@ public class ProductController {
 
     @GetMapping("/getProduct/{productId}")
     public ProductDto getProduct(@PathVariable Long productId){
-        return new ProductDto(1L, "Test product", "Test description", new BigDecimal("99.99"));
+        return new ProductDto(1L, "Test product", "Test description", new BigDecimal("99.99"), 1L);
     }
 
     @PostMapping(value = "createProduct")
@@ -27,7 +27,7 @@ public class ProductController {
 
     @PutMapping("updateProduct")
     public ProductDto updateProduct(@RequestBody ProductDto productDto) {
-        return new ProductDto(1L, "Edited test product", "Edited test description", new BigDecimal("99.99"));
+        return new ProductDto(1L, "Edited test product", "Edited test description", new BigDecimal("99.99"), 1L);
     }
 
     @DeleteMapping(value = "deleteProduct")
