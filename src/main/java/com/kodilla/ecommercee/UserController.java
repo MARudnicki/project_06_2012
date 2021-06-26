9f2eb19134cbd51a80389cdc7ab88c59daf10c97
Robert Gajewski <robert.gajewski@nask.pl>
2021-06-26 23:15:06 +0200

    UserController zwracanie danych testowych

--- src/main/java/com/kodilla/ecommercee/UserController.java
+++ src/main/java/com/kodilla/ecommercee/UserController.java
@@ -1,4 +1,46 @@
 package com.kodilla.ecommercee;
 
+import com.kodilla.dto.OrderDto;
+import com.kodilla.dto.UserDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/user")
+@RequiredArgsConstructor
 public class UserController {
+
+    @GetMapping("/getUsers")
+    public List<UserDto> getOrders() { return new ArrayList<>(); }
+
+    @GetMapping("/getUser/{userId}")
+    public UserDto getUser(@PathVariable Long userId) {
+        return new UserDto(2L, "Admin", true, new BigDecimal(96997));
+    }
+
+    @PostMapping(value = "createUser")
+    public void createUser(@RequestBody UserDto userDto) {
+        List<OrderDto> usersDto = userDto.getUser();
+    }
+
+    @PutMapping("updateUser")
+    public UserDto updateUser(@RequestBody UserDto userDto) {
+        return new UserDto(3L, "User", false, new BigDecimal(31251));
+    }
+
+    @PutMapping("blockUser")
+    public UserDto blockUser(@RequestBody UserDto userDto) {
+        return new UserDto(userDto.getId(), userDto.getUsername(), false, userDto.getUserKey());
+    }
+
+    @PutMapping("updateUserKey")
+    public UserDto updateUserKey(@RequestBody UserDto userDto) {
+        BigDecimal userKey = new BigDecimal(Math.random());;
+        return new UserDto(userDto.getId(), userDto.getUsername(), userDto.getStatus(), userKey);
+    }
+
 }
