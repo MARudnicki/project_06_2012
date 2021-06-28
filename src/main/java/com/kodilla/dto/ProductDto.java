b78cdbeb5b5e50bb82521a09dffaea31b1d93a0b
Robert Gajewski <robert.gajewski@nask.pl>
2021-06-28 17:29:40 +0200

    OrderController zwracanie danych testowych

--- src/main/java/com/kodilla/dto/ProductDto.java
+++ src/main/java/com/kodilla/dto/ProductDto.java
@@ -1,18 +1,18 @@
 package com.kodilla.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.Getter;
 
 import java.math.BigDecimal;
 
-@NoArgsConstructor
+@Getter
 @AllArgsConstructor
-@Data
 public class ProductDto {
+
     private Long id;
     private String name;
     private String description;
     private BigDecimal price;
     private Long groupId;
+
 }
