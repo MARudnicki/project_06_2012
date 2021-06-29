c212c89d845965d120e89c1d612193384465b6e0
andrzej-t <andrzej.tyrpa@gmail.com>
2021-06-29 10:40:47 +0200

    removed redundant class

--- src/main/java/com/kodilla/ecommercee/GroupRepository.java
+++
@@ -1,9 +0,0 @@
-//package com.kodilla.ecommercee;
-//
-//import com.kodilla.domain.Product;
-//import org.springframework.data.repository.CrudRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface GroupRepository extends CrudRepository<Product, Long> {
-//}
