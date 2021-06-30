250e8f34936229e391049809be27ec0abb7c1576
Robert Gajewski <robert.gajewski@nask.pl>
2021-06-30 22:41:57 +0200

    OrderController zwracanie danych testowych

--- src/main/java/com/kodilla/ecommercee/CartController.java
+++ src/main/java/com/kodilla/ecommercee/CartController.java
@@ -1,13 +1,12 @@
 package com.kodilla.ecommercee;
 
-import com.kodilla.dto.CartDto;
-import com.kodilla.dto.OrderDto;
-import com.kodilla.dto.ProductDto;
-import com.kodilla.dto.UserDto;
+import com.kodilla.dto.*;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
 @RequestMapping("/v1/cart")
@@ -15,15 +14,17 @@ public class CartController {
 
     @PostMapping("emptyShoppingCart")
     public void emptyShoppingCart(UserDto userDto) {
+        userDto.setCard(new CardDto());
+        userDto.getCard().setShoppingCart(new ArrayList<>());
     }
 
-    @GetMapping("getProductsShoppingCart")
+    @GetMapping("getProductsShoppingCart/{productId}")
     public List<ProductDto> getProductsShoppingCart(@RequestParam CartDto cartDto) {
         return cartDto.getShoppingCart();
     }
 
-    @PutMapping("addProduct")
-    public void addProduct(@RequestParam ProductDto product, CartDto cartDto) {
+    @PutMapping(value = "addProduct", consumes = APPLICATION_JSON_VALUE)
+    public void addProduct(@RequestBody ProductDto product, CartDto cartDto) {
         cartDto.getShoppingCart().add(product);
     }
 
