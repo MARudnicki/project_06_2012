8a0212d7d5fbc71aae1ad317369f9ad79072e139
Robert Gajewski <robert.gajewski@nask.pl>
2021-06-30 23:02:33 +0200

    Przeniesienie pakietu dto do ecommerce

--- src/main/java/com/kodilla/ecommercee/ProductController.java
+++ src/main/java/com/kodilla/ecommercee/ProductController.java
@@ -1,5 +1,6 @@
 package com.kodilla.ecommercee;
 
+import com.kodilla.ecommercee.dto.ProductDto;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
@@ -10,22 +11,22 @@ import java.util.List;
 public class ProductController {
 
     @GetMapping(value = "getProducts")
-    public List<ProductDto_> getProducts() {
+    public List<ProductDto> getProducts() {
         return new ArrayList<>();
     }
 
     @GetMapping("/getProduct/{productId}")
-    public ProductDto_ getProduct(@PathVariable Long productId){
-        return new ProductDto_(1L, "Test product", "Test description", new BigDecimal("99.99"), 2L);
+    public ProductDto getProduct(@PathVariable Long productId){
+        return new ProductDto(1L, "Test product", "Test description", new BigDecimal("99.99"), 2L);
     }
 
     @PostMapping(value = "createProduct")
-    public void createProduct(@RequestBody ProductDto_ productDto) {
+    public void createProduct(@RequestBody ProductDto productDto) {
     }
 
     @PutMapping("updateProduct")
-    public ProductDto_ updateProduct(@RequestBody ProductDto_ productDto) {
-        return new ProductDto_(1L, "Edited test product", "Edited test description", new BigDecimal("99.99"), 2L);
+    public ProductDto updateProduct(@RequestBody ProductDto productDto) {
+        return new ProductDto(1L, "Edited test product", "Edited test description", new BigDecimal("99.99"), 2L);
     }
 
     @DeleteMapping(value = "/deleteProduct/{productId}")
