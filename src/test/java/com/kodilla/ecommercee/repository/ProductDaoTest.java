adfee13943601e0ad3e970260e8a1f348dc7cad4
D4nielZ4k <78098047+D4nielZ4k@users.noreply.github.com>
2021-07-07 20:43:19 +0200

    naprawa budowania sie aplikacji

--- src/test/java/com/kodilla/ecommercee/repository/ProductDaoTest.java
+++ src/test/java/com/kodilla/ecommercee/repository/ProductDaoTest.java
@@ -3,13 +3,10 @@ package com.kodilla.ecommercee.repository;
 import com.kodilla.ecommercee.domain.Group;
 import com.kodilla.ecommercee.domain.Order;
 import com.kodilla.ecommercee.domain.Product;
-import com.kodilla.ecommercee.repository.ProductDao;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.math.BigDecimal;
 import java.util.Optional;
@@ -21,13 +18,13 @@ public class ProductDaoTest {
     @Autowired
     private ProductDao productDao;
 
-    @ExtendWith(SpringExtension.class)
+
     @Test
     public void testEntityProduct() {
 
         //Given
         Product product = new Product(1L, "test", "test",
-                new BigDecimal(500), new Group(),new Order());
+                new BigDecimal(500), new Group(), new Order());
         //When
         productDao.save(product);
         Optional<Product> readProduct = productDao.findById(1L);
