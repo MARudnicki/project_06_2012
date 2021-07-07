b28b16298ad1752d1fce374b7359b03c4fc29897
andrzej-t <andrzej.tyrpa@gmail.com>
2021-07-07 19:36:20 +0200

    Removing redundant import

--- src/test/java/com/kodilla/ecommercee/repository/UserTestSuite.java
+++ src/test/java/com/kodilla/ecommercee/repository/UserTestSuite.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
