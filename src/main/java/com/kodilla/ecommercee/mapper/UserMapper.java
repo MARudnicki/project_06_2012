3693c41f0c6e9d0c1212fed802601bcc548eb0a6
Robert Gajewski <robert.gajewski@nask.pl>
2021-07-12 18:17:34 +0200

    UserController - update implementation

---
+++ src/main/java/com/kodilla/ecommercee/mapper/UserMapper.java
@@ -0,0 +1,51 @@
+package com.kodilla.ecommercee.mapper;
+
+import com.kodilla.ecommercee.domain.Cart;
+import com.kodilla.ecommercee.domain.User;
+import com.kodilla.ecommercee.dto.CartDto;
+import com.kodilla.ecommercee.dto.UserDto;
+import com.kodilla.ecommercee.dto.UserKeyDto;
+import com.kodilla.ecommercee.exception.UserNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class UserMapper {
+
+    @Autowired
+    public UserMapper() {
+    }
+
+    public UserDto mapToUserDto(final User user){
+        UserKeyDto userKeyDto = new UserKeyDto(user.getUserKey(), System.currentTimeMillis() + 60*60*1000);
+        return new UserDto(
+                user.getId(),
+                user.getUsername(),
+                user.getStatus(),
+                new UserKeyDto(user.getUserKey(), userKeyDto.getValidUserKeyTime()),
+                new CartDto()
+        );
+    }
+
+    public List<UserDto> mapToUserDtoList(final List<User> userList) {
+        return userList.stream()
+                .map(this::mapToUserDto)
+                .collect(Collectors.toList());
+    }
+
+    public User mapToUser(UserDto userDto) throws UserNotFoundException {
+        return new User(
+                userDto.getId(),
+                userDto.getUsername(),
+                userDto.getStatus(),
+                userDto.getUserKey().getUserKey(),
+                null,
+                null
+        );
+    }
+
+}
