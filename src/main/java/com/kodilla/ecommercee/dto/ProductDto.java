1337ab6b35d870af4e52a1a66972293b07b86d8b
Jakub <jurak2012@gmail.com>
2021-07-13 15:20:18 +0200

    Added ProductDto default constructor

--- src/main/java/com/kodilla/ecommercee/dto/ProductDto.java
+++ src/main/java/com/kodilla/ecommercee/dto/ProductDto.java
@@ -2,10 +2,12 @@ package com.kodilla.ecommercee.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class ProductDto {
 
